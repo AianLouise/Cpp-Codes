637c93f96a0b63481a82948453492d10fc3dac1a
AianLouise <89790869+AianLouise@users.noreply.github.com>
2022-04-29 23:21:07 +0800

    Add files via upload

--- Act 3/Alfaro_Aian_Louise_A_2.cpp
+++ Act 3/Alfaro_Aian_Louise_A_2.cpp
@@ -7,71 +7,30 @@ using namespace std;
 
 int main(){
     char country[MAX];
+    char countryTable[MAX][MAX] = {"Brunei", "Cambodia", "East Timor", "Indonesia", "Laos", "Malaysia", "Myanmar", "Philippines", "Singapore", "Thailand", "Vietnam" };
+    char capitalTable[MAX][MAX] = {"Bandar Seri Begawan", "Phnom Penh", "Dili", "Jakarta", "Vientiane", "Kuala Lumpur", "Nay Pyi Daw", "Manila", "Singapore", "Bangkok", "Hanoi" };
     int c[MAX];
 
     cout << "COUNTRY NAME: " ;
     cin >> country;
     cout << "CAPITAL: ";
-    
-    c[0] = strcmpi(country, "Brunei");
-    c[1] = strcmpi(country, "Cambodia");
-    c[2] = strcmpi(country, "East Timor");
-    c[3] = strcmpi(country, "Indonesia");
-    c[4] = strcmpi(country, "Laos");
-    c[5] = strcmpi(country, "Malaysia");
-    c[6] = strcmpi(country, "Myanmar");
-    c[7] = strcmpi(country, "Philippines");
-    c[8] = strcmpi(country, "Singapore");
-    c[9] = strcmpi(country, "Thailand");
-    c[10] = strcmpi(country, "Vietnam");
 
-    if (c[0] == 0)
+    for (int i = 0; i < 11; i++)
     {
-        cout << "Bandar Seri Begawan";
+        c[i] = strcmpi(country, countryTable[i]);
     }
-    else if (c[1] == 0)
+    for (int i = 0; i < 11; i++)
     {
-        cout << "Phnom Penh";
-    }
-    else if (c[2] == 0)
-    {
-        cout << "Dili";
-    }
-    else if (c[3] == 0)
-    {
-        cout << "Jakarta";
-    }
-    else if (c[4] == 0)
-    {
-        cout << "Vientiane";
-    }
-    else if (c[5] == 0)
-    {
-        cout << "Kuala Lumpur";
-    }
-    else if (c[6] == 0)
-    {
-        cout << "Nay Pyi Daw";
-    }
-    else if (c[7] == 0)
-    {
-        cout << "Manila";
-    }
-    else if (c[8] == 0)
-    {
-        cout << "Singapore";
-    }
-    else if (c[9] == 0)
-    {
-        cout << "Bangkok";
-    }
-    else if (c[10] == 0)
-    {
-        cout << "Hanoi";
-    }
-    else
-    {
-        cout << country << " is not a South East Asian Country." ;
+        if (c[i] == 0)
+        {
+            cout << capitalTable[i];
+        }
+        else
+        {
+            cout << country << " is not South East Asian Country." ;
+            break;
+        }
+        
     }
     
 }   
